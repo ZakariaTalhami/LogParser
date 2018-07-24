aab21f182842b043c5385242fb91e936a327048b
Zakaria Talhami <Zakaria.a.a.talhami@gmail.com>
2018-07-24 13:07:06 +0300

    Fixed interfaces

--- src/main/java/com/example/demo/dao/ServiceDAO.java
+++ src/main/java/com/example/demo/dao/ServiceDAO.java
@@ -1,6 +1,9 @@
 package com.example.demo.dao;
 
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.example.demo.model.ServiceModel;
 
 public interface ServiceDAO {
@@ -9,4 +12,7 @@ public interface ServiceDAO {
 	public List<ServiceModel> findAll();
 	public ServiceModel findById(int id);
 	public ServiceModel findByName(String name);
+	
+	public boolean exist(String name);
+	public List<ServiceModel> updateServiceList();
 }
